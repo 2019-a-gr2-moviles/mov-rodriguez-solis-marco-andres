5f800d3035b16b3ef7e795e5014bf1e997ac3429
marco rodriguez <mar_rs_123@hotmail.com>
2019-06-27 16:03:06 -0500

    conexion http

---
+++ 02-android/MiAplicacion/app/src/main/java/com/example/miaplicacion/Empresa.kt
@@ -0,0 +1,15 @@
+package com.example.miaplicacion
+
+import java.util.*
+import kotlin.collections.ArrayList
+
+class Empresa(var id: Int, var nombre: String, var createdAt: Long, var updatedAt: Long, var usuariosDeEmpresa: ArrayList<UsuarioEmpresa>) {
+
+    var fechaCreacion: Date
+    var fechaActualizacion: Date
+
+    init {
+        fechaCreacion = Date(createdAt)
+        fechaActualizacion = Date(updatedAt)
+    }
+}
---
+++ 02-android/MiAplicacion/app/src/main/java/com/example/miaplicacion/UsuarioEmpresa.kt
@@ -0,0 +1,14 @@
+package com.example.miaplicacion
+
+import java.util.*
+
+class UsuarioEmpresa(var id: Int, var nombre: String, var createdAt: Long, var updatedAt: Long, var fkEmpresa: Int) {
+
+    var fechaCreacion: Date
+    var fechaActualizacion: Date
+
+    init {
+        fechaCreacion = Date(createdAt)
+        fechaActualizacion = Date(updatedAt)
+    }
+}
